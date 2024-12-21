a7ed3e0e741209d3005a9d5bb34f80f864168e1c
Hridayesh Adhikari <harshitadhikari99@gmail.com>
2024-12-21 11:10:55 +0530

    added hosted bakend url in frontend

--- blog_app_frontend/src/config/api.js
+++ blog_app_frontend/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL="http://localhost:2003"
+export const API_BASE_URL="https://peaceful-motivation-production.up.railway.app"
 
 const jwtToken=localStorage.getItem("token")
 
