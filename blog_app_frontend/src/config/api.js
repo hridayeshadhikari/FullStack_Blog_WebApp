fda1a9d00daa6e13a548f915502e44111079c1eb
Hridayesh Adhikari <harshitadhikari99@gmail.com>
2025-01-12 19:48:52 +0530

    changed backend base url

--- blog_app_frontend/src/config/api.js
+++ blog_app_frontend/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://peaceful-motivation-production.up.railway.app";
+export const API_BASE_URL = "https://ample-unity-production.up.railway.app";
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
