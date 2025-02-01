f665457250b86684e12e48e7e8398c7760f0718b
Hridayesh Adhikari <harshitadhikari99@gmail.com>
2025-02-01 11:12:54 +0530

    added dockerfile and changed backend deployment to render

---
+++ OAuth2AppBackend/src/test/java/com/example/oauthappbackend/OAuth2AppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuth2AppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- blog_app_frontend/src/config/api.js
+++ blog_app_frontend/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://ample-unity-production.up.railway.app";
+export const API_BASE_URL = "https://spring-boot-app-blogapp.onrender.com";
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
